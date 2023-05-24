d05cacd2d6f8aae86af2667e7d8886c59053fe24
seboubou <sebats@outlook.fr>
2023-05-24 11:32:43 +0200

    GET

--- userHandlers.js
+++ userHandlers.js
@@ -1,14 +1,29 @@
 const database = require("./database");
 
 const getUsers = (req, res) => {
+  let sql = "select * from users";
+  const sqlValues = [];
+
+  if (req.query.language != null) {
+    sql += " where language = ?";
+    sqlValues.push(req.query.language);
+  
+  if (req.query.city != null) {
+    sql += " and city = ?";
+    sqlValues.push(req.query.city);
+  }
+} else if (req.query.city != null) {
+  sql += " where city = ?";
+  sqlValues.push(req.query.city);
+}
   database
-    .query("SELECT * FROM users")
+    .query(sql, sqlValues)
     .then(([users]) => {
       res.json(users);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send("Error retrieving data from the database");
+      res.status(500).send("Error retrieving data from database");
     });
 };
 
